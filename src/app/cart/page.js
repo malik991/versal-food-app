99ee4c6b64bc94fce5c6040442fea6caa212e9d8
malik <mubi.emailid@gmail.com>
2024-05-11 09:53:17 +0500

    designed cart product component for cart page

--- src/app/cart/page.js
+++ src/app/cart/page.js
@@ -2,8 +2,7 @@
 import SectionHeaders from "@/components/layout/SectionHeaders";
 import { useContext, useState, useEffect } from "react";
 import { CartContext } from "../context/authProvide";
-import Image from "next/image";
-import { Trash } from "@/components/icons/Right";
+import CartProducts from "@/components/menu/CartProducts";
 import { cartProductPrice } from "../context/authProvide";
 import AddressInput from "@/components/layout/addressInput";
 import { useProfile } from "@/components/MyHooks/UseProfile";
@@ -115,45 +114,12 @@ export default function CartPage() {
           )}
           {cartProducts?.length > 0 &&
             cartProducts.map((product, index) => (
-              <div
+              <CartProducts
                 key={index}
-                className="flex gap-4 mb-2 border-b py-2 items-center"
-              >
-                <div className="w-24">
-                  <Image src={product.image} alt="" width={240} height={240} />
-                </div>
-                <div className="grow">
-                  <h3 className="font-semibold">{product.name}</h3>
-                  {product.size && (
-                    <div className="text-sm text-gray-700">
-                      <span>size: {product.size.name}</span>
-                    </div>
-                  )}
-                  {product.extras?.length > 0 && (
-                    <div className="text-sm text-gray-500">
-                      {product.extras.map((extra) => (
-                        <div>
-                          <span className="text-gray-500">
-                            {extra.name}: ${extra.price},
-                          </span>
-                        </div>
-                      ))}
-                    </div>
-                  )}
-                </div>
-                <div className="font-semibold text-lg mr-2">
-                  ${cartProductPrice(product)}
-                </div>
-                <div>
-                  <button
-                    type="button"
-                    onClick={() => removeProduct(index)}
-                    className="p-2"
-                  >
-                    <Trash className="w-5 h-5" />
-                  </button>
-                </div>
-              </div>
+                product={product}
+                index={index}
+                needToRemove={removeProduct}
+              />
             ))}
           {/* Display total price */}
           <div className="text-right pr-10 text-gray-500 mt-2 ">
---
+++ src/components/menu/CartProducts.js
@@ -0,0 +1,45 @@
+import Image from "next/image";
+import { Trash } from "@/components/icons/Right";
+import { cartProductPrice } from "../../app/context/authProvide";
+export default function CartProducts({ product, needToRemove, index }) {
+  return (
+    <div className="flex gap-4 mb-2 border-b py-2 items-center">
+      <div className="w-24">
+        <Image src={product.image} alt="" width={240} height={240} />
+      </div>
+      <div className="grow">
+        <h3 className="font-semibold">{product.name}</h3>
+        {product.size && (
+          <div className="text-sm text-gray-700">
+            <span>size: {product.size.name}</span>
+          </div>
+        )}
+        {product.extras?.length > 0 && (
+          <div className="text-sm text-gray-500">
+            {product.extras.map((extra) => (
+              <div>
+                <span className="text-gray-500">
+                  {extra.name}: ${extra.price},
+                </span>
+              </div>
+            ))}
+          </div>
+        )}
+      </div>
+      <div className="font-semibold text-lg mr-2">
+        ${cartProductPrice(product)}
+      </div>
+      {!!needToRemove && (
+        <div>
+          <button
+            type="button"
+            onClick={() => needToRemove(index)}
+            className="p-2"
+          >
+            <Trash className="w-5 h-5" />
+          </button>
+        </div>
+      )}
+    </div>
+  );
+}
